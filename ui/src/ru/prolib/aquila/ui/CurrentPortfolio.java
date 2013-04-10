83ffff92993e979bdf8049dd54ff9298c30dd8cd
huan.kaktus <huan@phpkaktus.net>
2013-04-11 03:43:28 +0400

    Such as previous

--- ui/src/ru/prolib/aquila/ui/CurrentPortfolio.java
+++ ui/src/ru/prolib/aquila/ui/CurrentPortfolio.java
@@ -1,11 +1,13 @@
 package ru.prolib.aquila.ui;
 
+import ru.prolib.aquila.core.EventListener;
 import ru.prolib.aquila.core.EventType;
+import ru.prolib.aquila.core.Starter;
 import ru.prolib.aquila.core.BusinessEntities.Portfolio;
 /**
  * $Id: CurrentPortfolio.java 525 2013-02-12 19:51:39Z huan.kaktus $
  */
-public interface CurrentPortfolio {
+public interface CurrentPortfolio extends Starter, EventListener {
 	
 	public Portfolio getCurrentPortfolio();
 	
