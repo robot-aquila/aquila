a2115214f82405b35614183e7c16375895575557
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2013-04-10 14:04:44 +0400

    core: setup position added allowed type

--- core/src/ru/prolib/aquila/core/BusinessEntities/SetupPosition.java
+++ core/src/ru/prolib/aquila/core/BusinessEntities/SetupPosition.java
@@ -41,6 +41,7 @@ public interface SetupPosition extends Cloneable {
 	 * Установить тип позиции.
 	 * <p>
 	 * @param value тип позиции
+	 * @throws IllegalArgumentException указан тип {@link PositionType#BOTH}
 	 */
 	public void setType(PositionType value);
 	
@@ -50,5 +51,58 @@ public interface SetupPosition extends Cloneable {
 	 * @return копия объекта
 	 */
 	public SetupPosition clone();
+	
+	/**
+	 * Установить разрешенный тип позиции.
+	 * <p>
+	 * Подразумевается:<p>
+	 * {@link PositionType#CLOSE} - запретить открытие позиций<br>
+	 * {@link PositionType#LONG} - разрешить только длиные позиции<br>
+	 * {@link PositionType#SHORT} - разрешить только короткие позиции<br>
+	 * {@link PositionType#BOTH} - разрешить открытие позиций либого типа<br>
+	 * <p>
+	 * @param value разрешенный тип
+	 */
+	public void setAllowedType(PositionType value);
+	
+	/**
+	 * Получить разрешенный тип позиции.
+	 * <p>
+	 * см. {@link SetupPosition#setAllowedType(PositionType)}.
+	 * <p>
+	 * @return разрешенный тип позиции
+	 */
+	public PositionType getAllowedType();
+	
+	/**
+	 * Проверить разрешение на открытие позиции указанного типа.
+	 * <p>
+	 * Для проверки используется настройка разрешенного типа позиции.
+	 * <p>
+	 * @param type тип для проверки {@link PositionType#LONG} или
+	 * {@link PositionType#SHORT}. Ответ на прочие значения всегда false.
+	 * @return true - разрешено открыть позицию данного типа, false - запрещено
+	 */
+	public boolean isOpenAllowed(PositionType type);
+	
+	/**
+	 * Проверить необходимость закрытия текущей позиции.
+	 * <p>
+	 * Проверяет нужно-ли закрыть текущую позицию в соответствии с настройками.
+	 * Учитывается целевой тип позиции и установленный разрешенный тип.
+	 * Когда текущий тип {@link PositionType#SHORT}, целевой тип
+	 * {@link PositionType#CLOSE} или {@link PositionType#LONG}, но при этом
+	 * разрешенный тип {@link PositionType#CLOSE} или только
+	 * {@link PositionType#SHORT}, то позицию следует закрыть. Для текущего
+	 * {@link PositionType#LONG} позиция закрывается, если целевой тип
+	 * {@link PositionType#CLOSE} или {@link PositionType#SHORT}, но при этом
+	 * разрешенный тип {@link PositionType#CLOSE} или только
+	 * {@link PositionType#LONG}. В остальных случаях закрывать позицию не
+	 * следует.
+	 * <p>
+	 * @param current тип текущей позиции
+	 * @return true - следует закрыть позицию, false - ничего делать не нужно
+	 */
+	public boolean isShouldClose(PositionType current);
 
 }
