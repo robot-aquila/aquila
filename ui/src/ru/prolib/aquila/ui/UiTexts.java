f0c80ae01907c503886918400ef5b997cd005d98
huan.kaktus <huan@phpkaktus.net>
2013-04-12 19:09:03 +0400

    Added interface texts base class

--- ui/src/ru/prolib/aquila/ui/UiTexts.java
+++ ui/src/ru/prolib/aquila/ui/UiTexts.java
@@ -1,11 +1,16 @@
 package ru.prolib.aquila.ui;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import org.ini4j.Wini;
+import org.apache.commons.lang3.StringUtils;
+import org.ini4j.InvalidFileFormatException;
+import org.ini4j.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * $Id: UiTexts.java 570 2013-03-12 00:03:15Z huan.kaktus $
@@ -13,38 +18,66 @@ import org.ini4j.Wini;
  */
 public class UiTexts {
 	
-	private Wini ini;
-	private final String[] fileName = {"shared", "uitexts.ini"};
+	private static Logger logger = LoggerFactory.getLogger(UiTexts.class);
+	private String defLng = "en_US";
+	private final String[] localesPath = {"shared", "lang"};
+	private String dirSeparator = System.getProperty("file.separator");
 	private Map<String, ClassLabels> labels = new HashMap<String, ClassLabels>();
+	private String lang;
 	
 	public UiTexts() {
 		super();		
 	}
 	
-	public void load() throws IOException {
-		ConfLoader loader = new ConfLoader(fileName);
-		ini = loader.load();
+	public UiTexts(String lang) {
+		super();
+		this.lang = lang;
 	}
 	
-	public ClassLabels get(String className) {
-		if(! labels.containsKey(className)) {
-			loadClassLabels(className);
+	public void load() {
+		loadLocale(defLng);
+		if(lang != null) {
+			loadLocale(lang);
 		}
-		return labels.get(className);
+	}
+	
+	private void loadLocale(String lang) {
+		String folderPath = StringUtils.join(localesPath, dirSeparator)+dirSeparator+lang;
+		File folder = new File(folderPath);
+		File[] files = folder.listFiles();
+		for(int i = 0; i < files.length; i++) {
+			try {
+				Options opt = new Options(files[i]);
+				String[] name = StringUtils.split(files[i].getName(), ".");
+				loadClassLabels(name[0], opt);
+				
+			} catch (InvalidFileFormatException e) {
+				logger.error("Invalid locale file format:{}", e.getMessage());
+			}catch(IOException e) {
+				logger.error(e.getMessage());
+			}
+		}
+	}
+	
+	public ClassLabels get(String name) {
+		if(! labels.containsKey(name)) {
+			labels.put(name, createClassLabel(name));
+		}
+		return labels.get(name);
 	}
 	
 	public void setClassLabels(String className, ClassLabels lbs) {
 		labels.put(className, lbs);
 	}
 	
-	private void loadClassLabels(String className) {
-		Properties props = new Properties();
-		Map<String, String> propData = ini.get(className);
-		if(propData != null) {
-			for(String key: propData.keySet()) {
-				props.setProperty(key, propData.get(key));
-			}
+	private void loadClassLabels(String className, Options opt) {
+		Properties props = get(className).getLabels();
+		for(String key: opt.keySet()) {
+			props.setProperty(key, opt.get(key));
 		}
-		labels.put(className, new ClassLabels(className, props));
+	}
+	
+	private ClassLabels createClassLabel(String name) {
+		return new ClassLabels(name);
 	}
 }
---
+++ ui/tests/ru/prolib/aquila/ui/UiTextsTest.java
@@ -0,0 +1,68 @@
+package ru.prolib.aquila.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * $Id$
+ */
+public class UiTextsTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ru.prolib.aquila.ui.UiTexts#UiTexts()}.
+	 */
+	@Test
+	public void testUiTexts() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ru.prolib.aquila.ui.UiTexts#UiTexts(java.lang.String)}.
+	 */
+	@Test
+	public void testUiTextsString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ru.prolib.aquila.ui.UiTexts#load()}.
+	 */
+	@Test
+	public void testLoad() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ru.prolib.aquila.ui.UiTexts#get(java.lang.String)}.
+	 */
+	@Test
+	public void testGet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ru.prolib.aquila.ui.UiTexts#setClassLabels(java.lang.String, ru.prolib.aquila.ui.ClassLabels)}.
+	 */
+	@Test
+	public void testSetClassLabels() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
