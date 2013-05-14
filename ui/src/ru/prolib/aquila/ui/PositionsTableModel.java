fed80502be3c60faa491c0554833fb00f26bf5ec
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2013-05-14 14:15:54 +0400

    ui: minor position table fix in changes of security model

--- ui/src/ru/prolib/aquila/ui/PositionsTableModel.java
+++ ui/src/ru/prolib/aquila/ui/PositionsTableModel.java
@@ -2,15 +2,9 @@ package ru.prolib.aquila.ui;
 
 import java.util.List;
 import java.util.Vector;
-
 import javax.swing.table.AbstractTableModel;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import ru.prolib.aquila.core.*;
 import ru.prolib.aquila.core.BusinessEntities.*;
-import ru.prolib.aquila.core.BusinessEntities.SecurityException;
 
 
 /**
@@ -20,7 +14,6 @@ public class PositionsTableModel extends AbstractTableModel implements
 		EventListener, Starter 
 {
 	private static final long serialVersionUID = 8731892367809895323L;
-	private static Logger logger = LoggerFactory.getLogger(PositionsTableModel.class);
 	private static final String COL_ACCOUNT = "COL_ACCOUNT";
 	private static final String COL_SEC = "COL_SEC";
 	private static final String COL_TYPE = "COL_TYPE";
@@ -72,12 +65,7 @@ public class PositionsTableModel extends AbstractTableModel implements
 		if(header[col] == COL_ACCOUNT) {
 			return p.getAccount();
 		}else if( header[col] == COL_SEC ) {			
-			try {
-				return p.getSecurity().getDescriptor();
-			}catch (SecurityException e) {
-				logger.error("SecurityException: ", e);
-				return "";
-			}
+			return p.getSecurity().getDescriptor();
 		} else if ( header[col] == COL_VAR_MARG ) {
 			return p.getVarMargin();
 		} else if (header[col] == COL_OPEN_VAL ) {
