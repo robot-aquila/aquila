96ecc02a16507a958721ac2968e0c35276406726
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2014-11-19 05:35:52 +0300

    probe: forts security handler (incomplete)

--- probe/src/ru/prolib/aquila/probe/internal/SecurityHandlerFORTS.java
+++ probe/src/ru/prolib/aquila/probe/internal/SecurityHandlerFORTS.java
@@ -2,11 +2,12 @@ package ru.prolib.aquila.probe.internal;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import ru.prolib.aquila.core.BusinessEntities.*;
+import ru.prolib.aquila.core.BusinessEntities.utils.BMUtils;
 import ru.prolib.aquila.core.data.Tick;
 import ru.prolib.aquila.probe.PROBETerminal;
 
 /**
- * Обработчик инструмента в рамках эмулятора.
+ * Обработчик инструмента типа фьючерс ФОРТС.
  * <p>
  */
 public class SecurityHandlerFORTS implements TickHandler {
@@ -25,18 +26,18 @@ public class SecurityHandlerFORTS implements TickHandler {
 
 	@Override
 	public void doInitialTask(Tick firstTick) {
-		// Здесь устанавливаем только первичные атрибуты.
 		security.setDisplayName(props.getDisplayName());
 		security.setLotSize(props.getLotSize());
 		security.setPrecision(props.getPricePrecision());
 		security.setMinStepSize(props.getMinStepSize());
-		security.setInitialPrice(firstTick.getValue());
-		// В первый раз запрашиваем расчет стоимости шага явно.
-		// В последующем, это будет выполняться в момент клиринга, планирование
-		// которого осуществляется на следующих этапах обработки данного тика.
+		double price = firstTick.getValue();
+		security.setInitialPrice(price);
 		security.setMinStepPrice(calculateMinStepPrice());
-		// Тоже самое с начальной маржой. 
 		security.setInitialMargin(calculateInitialMargin());
+		security.setOpenPrice(price);
+		security.setHighPrice(price);
+		security.setLowPrice(price);
+		security.setClosePrice(price);
 	}
 	
 	/**
@@ -65,18 +66,14 @@ public class SecurityHandlerFORTS implements TickHandler {
 	@Override
 	public void doDailyTask(Tick prevDateTick, Tick nextDateTick) {
 		terminal.schedule(new Runnable() {
-			@Override public void run() { eveningClearing(); } },
-			nextDateTick.getTime()
-				.withHourOfDay(18)
-				.withMinuteOfHour(50)
-				.withSecondOfMinute(0)
-				.withMillisOfDay(0));
+			@Override public void run() { eveningClearing(); }
+			}, nextDateTick.getTime().withTime(18, 55, 0, 0));
 	}
 	
 	@Override
 	public Runnable createTask(final Tick tick) {
 		return new Runnable() {
-			@Override public void run() { regularTick(tick); }
+			@Override public void run() { regularTrade(tick); }
 		};
 	}
 
@@ -85,7 +82,10 @@ public class SecurityHandlerFORTS implements TickHandler {
 	 * <p>
 	 */
 	private void eveningClearing() {
-		
+		security.setClosePrice(security.getLastPrice());
+		security.setInitialPrice(security.getLastPrice());
+		security.setInitialMargin(calculateInitialMargin());
+		security.setMinStepPrice(calculateMinStepPrice());
 	}
 	
 	/**
@@ -93,20 +93,18 @@ public class SecurityHandlerFORTS implements TickHandler {
 	 * <p>
 	 * @param tick тик данных
 	 */
-	private void regularTick(Tick tick) {
+	private void regularTrade(Tick tick) {
 		double price = tick.getValue();
-		Trade t = new Trade(terminal);
-		t.setDirection(Direction.BUY);
-		t.setPrice(price);
-		t.setQty(tick.getOptionalValueAsLong());
-		t.setSecurityDescriptor(security.getDescriptor());
-		t.setTime(tick.getTime());
-		t.setVolume(price / security.getMinStepSize() *
-				security.getMinStepPrice() * tick.getOptionalValueAsLong());
-		security.fireTradeEvent(t);
-		security.setHighPrice(Math.max(security.getHighPrice(), price));
-		security.setLowPrice(Math.min(security.getLowPrice(), price));
+		if ( security.getOpenPrice() == null ) {
+			security.setOpenPrice(price);
+			security.setHighPrice(price);
+			security.setLowPrice(price);
+		} else {
+			security.setHighPrice(Math.max(security.getHighPrice(), price));
+			security.setLowPrice(Math.min(security.getLowPrice(), price));
+		}
 		security.fireChangedEvent();
+		security.fireTradeEvent(new BMUtils().tradeFromTick(tick, security));
 	}
 	
 	@Override
--- probe/tests/ru/prolib/aquila/probe/internal/SecurityHandlerFORTSTest.java
+++ probe/tests/ru/prolib/aquila/probe/internal/SecurityHandlerFORTSTest.java
@@ -2,10 +2,28 @@ package ru.prolib.aquila.probe.internal;
 
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import org.easymock.IMocksControl;
 import org.joda.time.DateTime;
 import org.junit.*;
-import ru.prolib.aquila.core.BusinessEntities.*;
+
+import ru.prolib.aquila.core.Event;
+import ru.prolib.aquila.core.EventListener;
+import ru.prolib.aquila.core.BusinessEntities.EditableSecurity;
+import ru.prolib.aquila.core.BusinessEntities.Scheduler;
+import ru.prolib.aquila.core.BusinessEntities.SchedulerLocal;
+import ru.prolib.aquila.core.BusinessEntities.Security;
+import ru.prolib.aquila.core.BusinessEntities.SecurityDescriptor;
+import ru.prolib.aquila.core.BusinessEntities.SecurityEvent;
+import ru.prolib.aquila.core.BusinessEntities.SecurityTradeEvent;
+import ru.prolib.aquila.core.BusinessEntities.SecurityType;
+import ru.prolib.aquila.core.BusinessEntities.TaskHandler;
+import ru.prolib.aquila.core.BusinessEntities.utils.BMUtils;
 import ru.prolib.aquila.core.data.Tick;
 import ru.prolib.aquila.probe.PROBETerminal;
 
@@ -17,6 +35,7 @@ public class SecurityHandlerFORTSTest {
 	}
 	
 	private IMocksControl control;
+	private Scheduler scheduler;
 	private EditableSecurity security;
 	private PROBETerminal terminal;
 	private SecurityProperties props;
@@ -25,6 +44,7 @@ public class SecurityHandlerFORTSTest {
 	@Before
 	public void setUp() throws Exception {
 		control = createStrictControl();
+		scheduler = control.createMock(Scheduler.class);
 		terminal = new PROBETerminal("foo");
 		security = terminal.getEditableSecurity(descr);
 		props = new SecurityProperties();
@@ -35,36 +55,102 @@ public class SecurityHandlerFORTSTest {
 		props.setInitialMarginCalculationBase(0.15d);
 		props.setStepPriceCalculationBase(0.2d);
 		handler = new SecurityHandlerFORTS(terminal, security, props);
+		terminal.getEventSystem().getEventQueue().start();
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		terminal.getEventSystem().getEventQueue().stop();
+		terminal.getEventSystem().getEventQueue().join(1000);
 	}
 	
 	@Test
 	public void testSchemeVersions() throws Exception {
-		assertEquals(1, Security.VERSION);
+		assertEquals(2, Security.VERSION);
 	}
 	
 	@Test
 	public void testDoInitialTask() throws Exception {
 		handler.doInitialTask(new Tick(DateTime.now(), 142912d));
 		
+		assertEquals("RTS-future-12.14", security.getDisplayName());
+		assertEquals(1, security.getLotSize());
+		assertEquals(0, security.getPrecision());
+		assertEquals(10d, security.getMinStepSize(), 0.1d);
 		assertNull(security.getAskPrice());
 		assertNull(security.getAskSize());
 		assertNull(security.getBidPrice());
 		assertNull(security.getBidSize());
-		assertNull(security.getClosePrice());
-		assertEquals("RTS-future-12.14", security.getDisplayName());
-		assertNull(security.getHighPrice());
+		assertEquals(142912d, security.getClosePrice(), 0.01d);
+		assertEquals(142912d, security.getHighPrice(), 0.01d);
+		assertEquals(142912d, security.getLowPrice(), 0.01d);
+		assertEquals(142912d, security.getOpenPrice(), 0.01d);
 		assertEquals(142912d, security.getInitialMargin(), 0.01d);
 		assertEquals(142912d, security.getInitialPrice(), 0.01d);
 		assertNull(security.getLastPrice());
 		assertNull(security.getLastTrade());
-		assertEquals(1, security.getLotSize());
-		assertNull(security.getLowPrice());
 		assertNull(security.getMaxPrice());
 		assertNull(security.getMinPrice());
 		assertEquals(1d, security.getMinStepPrice(), 0.1d);
-		assertEquals(10d, security.getMinStepSize(), 0.1d);
+	}
+	
+	@Test
+	public void testDoFinalTask() throws Exception {
+		handler.doFinalTask(new Tick(DateTime.now(), 115240d));		
+	}
+	
+	@Test
+	public void testDoDailyTask() throws Exception {
+		final List<Runnable> tmp = new Vector<Runnable>();
+		DateTime expected = new DateTime(2014, 11, 18, 18, 55, 0, 0);
+		expect(scheduler.schedule(anyObject(Runnable.class), eq(expected)))
+			.andDelegateTo(new SchedulerLocal() {
+				@Override public TaskHandler schedule(Runnable r, DateTime t) {
+					tmp.add(r);
+					return null;
+				}
+		});
+		control.replay();
+		terminal.setScheduler(scheduler);
+		security.setLastPrice(112590d); // Используется как цена закрытия сессии
+		
+		handler.doDailyTask(null,
+				new Tick(new DateTime(2014, 11, 18, 15, 34, 29, 0), 138940d));
+		
+		control.verify();
+		tmp.get(0).run();
+		assertEquals(112590d, security.getClosePrice(), 0.01d);
+		assertNull(security.getHighPrice());
+		assertNull(security.getLowPrice());
 		assertNull(security.getOpenPrice());
-		assertEquals(0, security.getPrecision());
+		assertEquals(112590d, security.getInitialPrice(), 0.01d);
+		assertEquals(112590d, security.getInitialMargin(), 0.01d);
+		assertEquals(1d, security.getMinStepPrice(), 0.1d);
+	}
+	
+	@Test
+	public void testCreateTask() throws Exception {
+		DateTime time = DateTime.now();
+		final List<Event> expected = new Vector<Event>(),
+				actual = new Vector<Event>();
+		expected.add(new SecurityEvent(security.OnChanged(), security));
+		expected.add(new SecurityTradeEvent(security.OnTrade(), security,
+			new BMUtils().tradeFromTick(new Tick(time, 119540d, 120d), security)));
+		final CountDownLatch finished = new CountDownLatch(2);
+		EventListener listener = new EventListener() {
+			@Override public void onEvent(Event event) {
+				actual.add(event);
+				finished.countDown();
+			}
+		};
+		security.OnChanged().addListener(listener);
+		security.OnTrade().addListener(listener);
+		handler.doInitialTask(new Tick(null, 120140d));
+		
+		handler.createTask(new Tick(time, 119540d, 120d)).run();
+		
+		assertTrue(finished.await(100, TimeUnit.MILLISECONDS));
+		assertEquals(expected, actual);
 	}
 	
 	@Test
@@ -77,11 +163,6 @@ public class SecurityHandlerFORTSTest {
 	@Test
 	public void testEquals() {
 		fail("TODO: incomplete");
-	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
 	}
 
 }
