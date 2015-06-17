0af34bb0782fba9a01f1aed149d49b8eae2be4ef
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2015-06-17 11:14:44 +0300

    datatools: Data writer bugfix

--- datatools/src/main/java/ru/prolib/aquila/datatools/tickdatabase/simple/DataWriterImpl.java
+++ datatools/src/main/java/ru/prolib/aquila/datatools/tickdatabase/simple/DataWriterImpl.java
@@ -27,17 +27,28 @@ public class DataWriterImpl implements DataWriter {
 		if ( currentSegment == null ) {
 			currentSegment = segmentManager.openWriter(descr, date);
 		} else if ( ! date.equals(currentSegment.getDate()) ) {
-			segmentManager.close(currentSegment);
+			closeCurrentWriter();
 			currentSegment = segmentManager.openWriter(descr, date);
 		}
 		currentSegment.write(tick);
+	}
+	
+	private void closeCurrentWriter() throws GeneralException {
+		try {
+			segmentManager.close(currentSegment);
+		} finally {
+			currentSegment = null;
+		}
 	}
 
 	@Override
 	public void close() throws IOException {
 		if ( currentSegment != null ) {
-			currentSegment.close();
-			currentSegment = null;
+			try {
+				closeCurrentWriter();
+			} catch ( GeneralException e ) {
+				throw new IOException(e);
+			}
 		}
 	}
 
