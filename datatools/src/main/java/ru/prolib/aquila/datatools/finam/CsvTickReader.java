ab748062da93c6e05014daa3caba4636146d8bd5
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2015-06-19 14:11:44 +0300

    datatools: csv tick reader implemented

---
+++ datatools/src/main/java/ru/prolib/aquila/datatools/finam/CsvTickReader.java
@@ -0,0 +1,114 @@
+package ru.prolib.aquila.datatools.finam;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import com.csvreader.CsvReader;
+
+import ru.prolib.aquila.core.data.Aqiterator;
+import ru.prolib.aquila.core.data.DataException;
+import ru.prolib.aquila.core.data.Tick;
+
+/**
+ * Tick data segment reader.
+ * <p>
+ * Reads tick data from CSV file.
+ */
+public class CsvTickReader implements Aqiterator<Tick> {
+	private static final String LAST = "<LAST>";
+	private static final String VOL = "<VOL>";
+	private static final String TIME = "<TIME>";
+	private static final String MILLISECONDS = "<MILLISECONDS>";
+	private static final DateTimeFormatter timeFormat;
+	
+	static {
+		timeFormat = DateTimeFormat.forPattern("HHmmss");
+	}
+	
+	private final CsvReader csvReader;
+	private final LocalDate date;
+	private Tick curr;
+	private boolean closed = false;
+	private boolean hasMilliseconds = false;
+	
+	public CsvTickReader(CsvReader csvReader, LocalDate date) {
+		super();
+		this.csvReader = csvReader;
+		this.date = date;
+	}
+	
+	public CsvTickReader(InputStream inputStream, LocalDate date) {
+		this(new CsvReader(new InputStreamReader(inputStream)), date);
+	}
+	
+	public CsvReader getCsvReader() {
+		return csvReader;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public boolean isClosed() {
+		return closed;
+	}
+	
+	public boolean hasMilliseconds() {
+		return hasMilliseconds;
+	}
+	
+	public void readHeader() throws IOException {
+		if ( csvReader.readHeaders() ) {
+			String headers[] = csvReader.getHeaders();
+			for ( String x : headers ) {
+				if ( x.equals(MILLISECONDS) ) {
+					hasMilliseconds = true;
+					break;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void close() {
+		csvReader.close();
+		closed = true;
+	}
+
+	@Override
+	public Tick item() throws DataException {
+		if ( curr == null || closed ) {
+			throw new DataException("No data under cursor");
+		}
+		return curr;
+	}
+
+	@Override
+	public boolean next() throws DataException {
+		if ( closed ) {
+			return false;
+		}
+		try {
+			if ( ! csvReader.readRecord() ) {
+				close();
+				return false;
+			}
+			DateTime dt = date.toDateTime(timeFormat.parseLocalTime(csvReader.get(TIME)));
+			if ( hasMilliseconds ) {
+				dt = dt.plus(Long.parseLong(csvReader.get(MILLISECONDS)));
+			}
+			curr = new Tick(dt, Double.parseDouble(csvReader.get(LAST)),
+					Double.parseDouble(csvReader.get(VOL)));
+			return true;
+		} catch ( Exception e ) {
+			throw new DataException(e);
+		}
+	}
+
+}
---
+++ datatools/src/test/java/ru/prolib/aquila/datatools/finam/CsvTickReaderTest.java
@@ -0,0 +1,271 @@
+package ru.prolib.aquila.datatools.finam;
+
+import static org.junit.Assert.*;
+import static org.easymock.EasyMock.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.easymock.IMocksControl;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.junit.*;
+
+import ru.prolib.aquila.core.data.DataException;
+import ru.prolib.aquila.core.data.Tick;
+
+import com.csvreader.CsvReader;
+
+public class CsvTickReaderTest {
+	private IMocksControl control;
+	private CsvReader csvReaderMock;
+	private LocalDate date;
+	private CsvTickReader reader;
+
+	@Before
+	public void setUp() throws Exception {
+		control = createStrictControl();
+		csvReaderMock = control.createMock(CsvReader.class);
+		date = new LocalDate(1998, 9, 1);
+		reader = new CsvTickReader(csvReaderMock, date);
+	}
+	
+	private InputStream createTestInput() {
+		return createTestInput("<TIME>,<LAST>,<VOL>,<MILLISECONDS>\n"
+			+ "100000,100.02,105,957\n"
+			+ "100001,100.18,102,13\n"
+			+ "100002,100.96,238,512\n");
+	}
+	
+	private InputStream createTestInput(String data) {
+		return new ByteArrayInputStream(data.getBytes());
+	}
+	
+	private CsvTickReader createTestReader(String data)
+			throws IOException
+	{
+		return createTestReader(data, false);
+	}
+	
+	private CsvTickReader createTestReader(String data, boolean skipHeader)
+			throws IOException
+	{
+		CsvTickReader x = new CsvTickReader(createTestInput(data), date);
+		if ( ! skipHeader ) {
+			x.readHeader();
+		}
+		return x;
+	}
+	
+	private CsvTickReader createTestReader()
+			throws IOException
+	{
+		return createTestReader(false);
+	}
+	
+	private CsvTickReader createTestReader(boolean skipHeader)
+			throws IOException
+	{
+		CsvTickReader x = new CsvTickReader(createTestInput(), date);
+		if ( ! skipHeader ) {
+			x.readHeader();
+		}
+		return x;
+	}
+	
+	@Test
+	public void testCtor2() throws Exception {
+		assertSame(csvReaderMock, reader.getCsvReader());
+		assertEquals(date, reader.getDate());
+		assertFalse(reader.isClosed());
+	}
+	
+	@Test
+	public void testClose() throws Exception {
+		csvReaderMock.close();
+		control.replay();
+		
+		reader.close();
+		
+		control.verify();
+		assertTrue(reader.isClosed());
+	}
+	
+	@Test
+	public void testReadHeader_SkipIfNoHeaders() throws Exception {
+		expect(csvReaderMock.readHeaders()).andReturn(false);
+		control.replay();
+		
+		reader.readHeader();
+		
+		control.verify();
+		assertFalse(reader.hasMilliseconds());
+	}
+	
+	@Test
+	public void testReadHeader_WithMillisecondsColumn() throws Exception {
+		String headers[] = {  "<CHARLIE>", "<MILLISECONDS>", "<ZULU>" }; 
+		expect(csvReaderMock.readHeaders()).andReturn(true);
+		expect(csvReaderMock.getHeaders()).andReturn(headers);
+		control.replay();
+		
+		reader.readHeader();
+		
+		control.verify();
+		assertTrue(reader.hasMilliseconds());
+	}
+
+	@Test
+	public void testReadHeader_WithoutMillisecondsColumn() throws Exception {
+		String headers[] = {  "<CHARLIE>", "<VOL>", "<ZULU>" }; 
+		expect(csvReaderMock.readHeaders()).andReturn(true);
+		expect(csvReaderMock.getHeaders()).andReturn(headers);
+		control.replay();
+		
+		reader.readHeader();
+		
+		control.verify();
+		assertFalse(reader.hasMilliseconds());
+	}
+	
+	@Test
+	public void testNext_FalseIfClosed() throws Exception {
+		reader = createTestReader();
+		reader.close();
+		
+		assertFalse(reader.next());
+	}
+	
+	@Test
+	public void testNext_FalseIfNoMoreRecords() throws Exception {
+		reader = createTestReader();
+		while ( reader.next() ) { }
+		
+		assertTrue(reader.isClosed());
+		assertFalse(reader.next());
+	}
+	
+	@Test
+	public void testNext_Ok_WithoutMillis() throws Exception {
+		reader = createTestReader("<TIME>,<LAST>,<VOL>\n"
+				+ "100000,100.02,105\n"
+				+ "100001,100.18,102\n"
+				+ "100002,100.96,238\n");
+		
+		assertTrue(reader.next());
+		Tick exp = new Tick(date.toDateTime(new LocalTime(10, 0, 0)), 100.02, 105); 
+		assertEquals(exp, reader.item());
+		
+		assertTrue(reader.next());
+		exp = new Tick(date.toDateTime(new LocalTime(10, 0, 1)), 100.18, 102); 
+		assertEquals(exp, reader.item());
+	}
+
+	@Test
+	public void testNext_Ok_WithMillis() throws Exception {
+		reader = createTestReader("<TIME>,<LAST>,<VOL>,<MILLISECONDS>\n"
+				+ "100000,100.02,105,999\n"
+				+ "100001,100.18,102,21\n"
+				+ "100002,100.96,238,4\n");
+		
+		assertTrue(reader.next());
+		Tick exp = new Tick(date.toDateTime(new LocalTime(10, 0, 0, 999)), 100.02, 105); 
+		assertEquals(exp, reader.item());
+		
+		assertTrue(reader.next());
+		exp = new Tick(date.toDateTime(new LocalTime(10, 0, 1,  21)), 100.18, 102); 
+		assertEquals(exp, reader.item());
+	}
+	
+	@Test (expected=DataException.class)
+	public void testNext_ReaderException() throws Exception {
+		expect(csvReaderMock.readRecord()).andThrow(new IOException("test"));
+		control.replay();
+		
+		reader.next();
+	}
+	
+	@Test (expected=DataException.class)
+	public void testNext_TimeParseException() throws Exception {
+		reader = createTestReader("<TIME>,<LAST>,<VOL>,<MILLISECONDS>\n"
+				+ "zzzxxx,100.02,105,999\n");
+		
+		reader.next();
+	}
+	
+	@Test (expected=DataException.class)
+	public void testNext_NumberParseException() throws Exception {
+		reader = createTestReader("<TIME>,<LAST>,<VOL>,<MILLISECONDS>\n"
+				+ "100000,foo,105,999\n");
+		
+		reader.next();
+	}
+	
+	@Test (expected=DataException.class)
+	public void testItem_ThrowsBeforeStart() throws Exception {
+		reader = createTestReader();
+		
+		reader.item();
+	}
+	
+	@Test (expected=DataException.class)
+	public void testItem_ThrowsAfterEnd() throws Exception {
+		reader = createTestReader();
+		while ( reader.next() ) { }
+
+		reader.item();
+	}
+	
+	@Test (expected=DataException.class)
+	public void testItem_ThrowsIfClosed() throws Exception {
+		reader = createTestReader();
+		reader.next();
+		reader.close();
+		
+		reader.item();
+	}
+	
+	@Test
+	public void testItem_WithMillis() throws Exception {
+		String data = "<TIME>,<LAST>,<VOL>,<MILLISECONDS>\n"
+				+ "100000,100.02,105,957\n"
+				+ "100001,100.18,102,13\n"
+				+ "100002,100.96,238,512\n";
+		reader = createTestReader(data);
+		
+		Tick expected[] = {
+			new Tick(date.toDateTime(new LocalTime(10, 0, 0, 957)), 100.02, 105),
+			new Tick(date.toDateTime(new LocalTime(10, 0, 1,  13)), 100.18, 102),
+			new Tick(date.toDateTime(new LocalTime(10, 0, 2, 512)), 100.96, 238),
+		};
+		for ( Tick e : expected ) {
+			assertTrue(reader.next());
+			assertEquals(e, reader.item());
+		}
+		reader.close();
+	}
+	
+	@Test
+	public void testItem_WithoutMillis() throws Exception {
+		String data = "<TIME>,<LAST>,<VOL>\n"
+				+ "100000,100.02,105\n"
+				+ "100001,100.18,102\n"
+				+ "100002,100.96,238\n"
+				+ "100005,100.42,500\n";
+		reader = createTestReader(data);
+		
+		Tick expected[] = {
+			new Tick(date.toDateTime(new LocalTime(10, 0, 0)), 100.02, 105),
+			new Tick(date.toDateTime(new LocalTime(10, 0, 1)), 100.18, 102),
+			new Tick(date.toDateTime(new LocalTime(10, 0, 2)), 100.96, 238),
+			new Tick(date.toDateTime(new LocalTime(10, 0, 5)), 100.42, 500),
+		};
+		for ( Tick e : expected ) {
+			assertTrue(reader.next());
+			assertEquals(e, reader.item());
+		}
+		reader.close();
+	}
+
+}
