b608d36d5274488610f09066288904e8cae45dea
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2015-09-16 09:20:22 +0300

    core: data storage removed

--- core/src/main/java/ru/prolib/aquila/core/data/DataStorage.java
+++
@@ -1,9 +0,0 @@
-package ru.prolib.aquila.core.data;
-
-/**
- * Интерфейс фасада комбинированного хранилища данных.
- */
-@Deprecated
-public interface DataStorage extends TickIteratorStorage, TickTemporalStorage {
-	
-}
