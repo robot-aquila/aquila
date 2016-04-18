4d07ad47b0e2d4f4ea9d539ccb63879685b1d1dc
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2016-04-18 14:08:48 +0300

    core: new order type methods added

--- core/src/main/java/ru/prolib/aquila/core/BusinessEntities/OrderType.java
+++ core/src/main/java/ru/prolib/aquila/core/BusinessEntities/OrderType.java
@@ -1,20 +1,51 @@
 package ru.prolib.aquila.core.BusinessEntities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Тип заявки.
+ * Order type.
  * <p>
  * 2012-05-30<br>
  * $Id: OrderType.java 542 2013-02-23 04:15:34Z whirlwind $
  */
 public class OrderType {
+	private static final Map<String, OrderType> types = new HashMap<>();
+	
+	public synchronized static OrderType registerType(String code) {
+		if ( types.containsKey(code) ) {
+			throw new IllegalArgumentException("Type already exists: " + code);
+		}
+		OrderType type = new OrderType(code);
+		types.put(code, type);
+		return type;
+	}
+	
+	public synchronized static OrderType registerType(OrderType type) {
+		String code = type.getCode();
+		if ( types.containsKey(code) ) {
+			throw new IllegalArgumentException("Type already exists: " + code);
+		}
+		types.put(code, type);
+		return type;
+	}
+	
+	public synchronized static OrderType byCode(String code) {
+		OrderType type = types.get(code);
+		if ( type == null ) {
+			throw new IllegalArgumentException("Type not exists: " + code);
+		}
+		return type;
+	}
+	
 	/**
-	 * Лимитная.
+	 * Limit order.
 	 */
-	public static final OrderType LIMIT = new OrderType("Limit");
+	public static final OrderType LIMIT = registerType("Limit");
 	/**
-	 * Рыночная.
+	 * Market order.
 	 */
-	public static final OrderType MARKET = new OrderType("Market");
+	public static final OrderType MARKET = registerType("Market");
 	
 	private final String code;
 	
@@ -23,13 +54,17 @@ public class OrderType {
 	 * <p>
 	 * @param code код типа
 	 */
-	private OrderType(String code) {
+	protected OrderType(String code) {
 		super();
 		this.code = code;
 	}
 	
 	@Override
 	public String toString() {
+		return code;
+	}
+	
+	public String getCode() {
 		return code;
 	}
 
--- core/src/test/java/ru/prolib/aquila/core/BusinessEntities/OrderTypeTest.java
+++ core/src/test/java/ru/prolib/aquila/core/BusinessEntities/OrderTypeTest.java
@@ -11,10 +11,65 @@ import org.junit.*;
  */
 public class OrderTypeTest {
 	
+	static class NewType extends OrderType {
+
+		protected NewType(String code) {
+			super(code);
+		}
+		
+	}
+	
 	@Test
 	public void testConstants() throws Exception {
 		assertEquals("Limit", OrderType.LIMIT.toString());
 		assertEquals("Market", OrderType.MARKET.toString());
+	}
+	
+	@Test
+	public void testByCode() throws Exception {
+		assertSame(OrderType.LIMIT, OrderType.byCode("Limit"));
+		assertSame(OrderType.MARKET, OrderType.byCode("Market"));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testByCode_ThrowsIfNotExists() {
+		OrderType.byCode("zulu24");
+	}
+	
+	@Test
+	public void testRegisterType1_Str() throws Exception {
+		OrderType type3 = OrderType.registerType("Type3");
+		OrderType type4 = OrderType.registerType("Type4");
+		
+		assertNotNull(type3);
+		assertNotNull(type4);
+		
+		assertSame(type3, OrderType.byCode("Type3"));
+		assertSame(type4, OrderType.byCode("Type4"));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testRegisterType1_Str_ThrowsIfAlreadyExists() {
+		OrderType.registerType("MyStringType");
+		OrderType.registerType("MyStringType");
+	}
+	
+	@Test
+	public void testRegisterType1_Obj() throws Exception {
+		NewType type5 = new NewType("Type5");
+		NewType type6 = new NewType("Type6");
+		
+		assertSame(type5, OrderType.registerType(type5));
+		assertSame(type6, OrderType.registerType(type6));
+		
+		assertSame(type5, OrderType.byCode("Type5"));
+		assertSame(type6, OrderType.byCode("Type6"));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testRegisterType1_Obj_ThrowsIfAlreadyExists() {
+		OrderType.registerType(new NewType("Type7"));
+		OrderType.registerType(new NewType("Type7"));
 	}
 
 }
