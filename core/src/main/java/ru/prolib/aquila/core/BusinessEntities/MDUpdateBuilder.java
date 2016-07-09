b3cca7d308f1b7f0260d7246cb6c7ff8dadd1e6c
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2016-07-09 20:33:01 +0300

    core: MDUpdate builder implemented

---
+++ core/src/main/java/ru/prolib/aquila/core/BusinessEntities/MDUpdateBuilder.java
@@ -0,0 +1,57 @@
+package ru.prolib.aquila.core.BusinessEntities;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MDUpdateBuilder {
+	private MDUpdateType type = MDUpdateType.REFRESH;
+	private Symbol symbol;
+	private Instant time = Instant.EPOCH;
+	private List<MDUpdateRecord> records = new ArrayList<>();
+
+	public MDUpdateBuilder(Symbol symbol) {
+		this.symbol = symbol;
+	}
+
+	public MDUpdateBuilder() {
+		this(null);
+	}
+	
+	public MDUpdateBuilder withType(MDUpdateType type) {
+		this.type = type;
+		return this;
+	}
+	
+	public MDUpdateBuilder withSymbol(Symbol symbol) {
+		this.symbol = symbol;
+		return this;
+	}
+	
+	public MDUpdateBuilder withTime(Instant time) {
+		this.time = time;
+		return this;
+	}
+	
+	public MDUpdateBuilder addAsk(double price, long size) {
+		records.add(new MDUpdateRecordImpl(Tick.of(TickType.ASK, time, price, size), MDTransactionType.ADD));
+		return this;
+	}
+	
+	public MDUpdateBuilder addBid(double price, long size) {
+		records.add(new MDUpdateRecordImpl(Tick.of(TickType.BID, time, price, size), MDTransactionType.ADD));
+		return this;
+	}
+	
+	public MDUpdate buildMDUpdate() {
+		if ( symbol == null ) {
+			throw new IllegalArgumentException("Symbol must be specified");
+		}
+		MDUpdateImpl update = new MDUpdateImpl(new MDUpdateHeaderImpl(type, time, symbol));
+		for ( MDUpdateRecord r : records ) {
+			update.addRecord(r.getTick(), r.getTransactionType());
+		}
+		return update;
+	}
+	
+}
---
+++ core/src/test/java/ru/prolib/aquila/core/BusinessEntities/MDUpdateBuilderTest.java
@@ -0,0 +1,113 @@
+package ru.prolib.aquila.core.BusinessEntities;
+
+import static org.junit.Assert.*;
+
+import java.time.Instant;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MDUpdateBuilderTest {
+	private static final Symbol symbol1 = new Symbol("AAPL");
+	private static final Symbol symbol2 = new Symbol("MSFT");
+	private MDUpdateBuilder builder;
+	private MDUpdateHeader expectedHeader;
+	private MDUpdateImpl expected;
+
+	@Before
+	public void setUp() throws Exception {
+		expectedHeader = null;
+		expected = null;
+		builder = new MDUpdateBuilder(symbol1);
+	}
+	
+	@Test
+	public void testCtor0() {
+		builder = new MDUpdateBuilder();
+		builder.withSymbol(symbol2);
+		
+		MDUpdate actual = builder.buildMDUpdate();
+		
+		expectedHeader = new MDUpdateHeaderImpl(MDUpdateType.REFRESH, Instant.EPOCH, symbol2);
+		expected = new MDUpdateImpl(expectedHeader);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testCtor1() {
+		builder = new MDUpdateBuilder(symbol1);
+		
+		MDUpdate actual = builder.buildMDUpdate();
+		
+		expectedHeader = new MDUpdateHeaderImpl(MDUpdateType.REFRESH, Instant.EPOCH, symbol1);
+		expected = new MDUpdateImpl(expectedHeader);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testWithType() throws Exception {
+		assertSame(builder, builder.withType(MDUpdateType.UPDATE));
+		
+		MDUpdate actual = builder.buildMDUpdate();
+		
+		expectedHeader = new MDUpdateHeaderImpl(MDUpdateType.UPDATE, Instant.EPOCH, symbol1);
+		expected = new MDUpdateImpl(expectedHeader);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testWithSymbol() throws Exception {
+		assertSame(builder, builder.withSymbol(symbol2));
+		
+		MDUpdate actual = builder.buildMDUpdate();
+		
+		expectedHeader = new MDUpdateHeaderImpl(MDUpdateType.REFRESH, Instant.EPOCH, symbol2);
+		expected = new MDUpdateImpl(expectedHeader);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testWithTime() throws Exception {
+		assertSame(builder, builder.withTime(Instant.parse("2016-07-09T20:17:00Z")));
+		
+		MDUpdate actual = builder.buildMDUpdate();
+		
+		expectedHeader = new MDUpdateHeaderImpl(MDUpdateType.REFRESH,
+				Instant.parse("2016-07-09T20:17:00Z"), symbol1);
+		expected = new MDUpdateImpl(expectedHeader);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testAddAsk() throws Exception {
+		Instant time = Instant.parse("2016-07-09T20:24:30Z");
+		assertSame(builder, builder.withTime(time)
+				.addAsk(120.01d, 1000L)
+				.addAsk(120.05d, 2000L));
+		
+		MDUpdate actual = builder.buildMDUpdate();
+		
+		expectedHeader = new MDUpdateHeaderImpl(MDUpdateType.REFRESH, time, symbol1);
+		expected = new MDUpdateImpl(expectedHeader);
+		expected.addRecord(Tick.of(TickType.ASK, time, 120.01d, 1000L), MDTransactionType.ADD);
+		expected.addRecord(Tick.of(TickType.ASK, time, 120.05d, 2000L), MDTransactionType.ADD);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testAddBid() throws Exception {
+		Instant time = Instant.parse("2016-07-09T20:26:45Z");
+		assertSame(builder, builder.withTime(time)
+				.addBid(150.38d, 800L)
+				.addBid(151.02d, 400L));
+		
+		MDUpdate actual = builder.buildMDUpdate();
+		
+		expectedHeader = new MDUpdateHeaderImpl(MDUpdateType.REFRESH, time, symbol1);
+		expected = new MDUpdateImpl(expectedHeader);
+		expected.addRecord(Tick.of(TickType.BID, time, 150.38d, 800L), MDTransactionType.ADD);
+		expected.addRecord(Tick.of(TickType.BID, time, 151.02d, 400L), MDTransactionType.ADD);
+		assertEquals(expected, actual);
+	}
+
+}
