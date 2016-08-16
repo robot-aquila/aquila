8a4ef83eca55530c4cd4963a589fa2b79dee2c1e
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2016-08-16 22:03:53 +0300

    web-utils: intermediate

--- web-utils/src/main/java/ru/prolib/aquila/web/utils/FileDownloader.java
+++ web-utils/src/main/java/ru/prolib/aquila/web/utils/FileDownloader.java
@@ -8,6 +8,16 @@ import java.net.URI;
  */
 public interface FileDownloader {
 
-	public void download(URI uri, OutputStream output) throws WUIOException;
+	/**
+	 * Download a file.
+	 * <p>
+	 * @param uri - URI to download
+	 * @param output - target stream
+	 * @throws WUInvalidResponseException - response validation failed
+	 * @throws WUProtocolException - application protocol exception
+	 * @throws WUIOException - lower-level IO exception
+	 */
+	public void download(URI uri, OutputStream output)
+		throws WUInvalidResponseException, WUProtocolException, WUIOException;
 
 }
