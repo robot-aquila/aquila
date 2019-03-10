f387eef15889913dd3c3a87fc00f6999286e65e3
aquila <robot-aquila@hotmail.com>
2019-03-10 11:47:51 +0300

    qforts improvements
    - dump object states added to transaction service on execution rejection

--- qforts/src/main/java/ru/prolib/aquila/qforts/impl/QFTransactionService.java
+++ qforts/src/main/java/ru/prolib/aquila/qforts/impl/QFTransactionService.java
@@ -2,17 +2,28 @@ package ru.prolib.aquila.qforts.impl;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ru.prolib.aquila.core.BusinessEntities.CDecimal;
 import ru.prolib.aquila.core.BusinessEntities.EditableOrder;
 import ru.prolib.aquila.core.BusinessEntities.EditablePortfolio;
 import ru.prolib.aquila.core.BusinessEntities.EditablePosition;
+import ru.prolib.aquila.core.BusinessEntities.Order;
 import ru.prolib.aquila.core.BusinessEntities.OrderStatus;
+import ru.prolib.aquila.core.BusinessEntities.Portfolio;
 import ru.prolib.aquila.core.BusinessEntities.Position;
 import ru.prolib.aquila.core.BusinessEntities.Security;
 import ru.prolib.aquila.core.concurrency.Lockable;
 import ru.prolib.aquila.core.concurrency.MultilockBuilderBE;
 
 public class QFTransactionService {
+	private static final Logger logger;
+	
+	static {
+		logger = LoggerFactory.getLogger(QFTransactionService.class);
+	}
+	
 	private final QFObjectRegistry registry;
 	private final AtomicLong seqExecutionID;
 	private final QFCalculator calculator;
@@ -89,11 +100,12 @@ public class QFTransactionService {
 	{
 		EditablePortfolio portfolio = (EditablePortfolio) order.getPortfolio();
 		Security security = order.getSecurity();
+		Position position = order.getPosition();
 		Lockable lock = assembler.createMultilock(new MultilockBuilderBE()
 				.add(order)
 				.add(security)
 				.add(portfolio)
-				.add(order.getPosition()));
+				.add(position));
 		lock.lock();
 		try {
 			if ( ! registry.isRegistered(order) ) {
@@ -104,6 +116,14 @@ public class QFTransactionService {
 					oeu.getPositionVolumeChange(), price);
 			int r = validator.canChangePositon(pcu);
 			if ( r != QFResult.OK ) {
+				dumpPortfolioState(portfolio);
+				dumpPositionState(position);
+				dumpSecurityState(security);
+				dumpOrderState(order);
+				logger.debug("Execution parameters ----------------------------");
+				logger.debug("Exec. Price: {}", price);
+				logger.debug(" Exec. Vol.: {}", volume);
+				
 				// TODO: Convert result code to message
 				throw new QFValidationException("Insufficient funds", r);
 			}
@@ -216,6 +236,71 @@ public class QFTransactionService {
 		} finally {
 			lock.unlock();
 		}
+	}
+
+	private void dumpPortfolioState(Portfolio portfolio) {
+		logger.debug("Order execution rejected ------------------------");
+		logger.debug("Portfolio state");
+		logger.debug("    Account: {}", portfolio.getAccount());
+		logger.debug("     Equity: {}", portfolio.getEquity());
+		logger.debug("    Balance: {}", portfolio.getBalance());
+		logger.debug("Used Margin: {}", portfolio.getUsedMargin());
+		logger.debug("Free Margin: {}", portfolio.getFreeMargin());
+		logger.debug("        P&L: {}", portfolio.getProfitAndLoss());
+		logger.debug("     Assets: {}", portfolio.getAssets());
+		logger.debug("Liabilities: {}", portfolio.getLiabilities());
+	}
+	
+	private void dumpOrderState(Order order) {
+		logger.debug("Order state -------------------------------------");
+		logger.debug("         ID: {}", order.getID());
+		logger.debug("    Account: {}", order.getAccount());
+		logger.debug("     Symbol: {}", order.getSymbol());
+		logger.debug("       Type: {}", order.getType());
+		logger.debug("     Action: {}", order.getAction());
+		logger.debug("      Price: {}", order.getPrice());
+		logger.debug("    Comment: {}", order.getComment());
+		logger.debug("   Sys. Msg: {}", order.getSystemMessage());
+		logger.debug("        UDL: {}", order.getUserDefinedLong());
+		logger.debug("        UDS: {}", order.getUserDefinedString());
+		logger.debug("       Time: {}", order.getTime());
+		logger.debug("  Time Done: {}", order.getTimeDone());
+		logger.debug("     Status: {}", order.getStatus());
+		logger.debug("  Init. Vol: {}", order.getInitialVolume());
+		logger.debug("  Curr. Vol: {}", order.getCurrentVolume());
+		logger.debug("  Exec. Val: {}", order.getExecutedValue());		
+	}
+	
+	private void dumpPositionState(Position position) {
+		logger.debug("Position state ----------------------------------");
+		logger.debug("    Account: {}", position.getAccount());
+		logger.debug("     Symbol: {}", position.getSymbol());
+		logger.debug(" Curr. Vol.: {}", position.getCurrentVolume());
+		logger.debug(" Open Price: {}", position.getOpenPrice());
+		logger.debug("Curr. Price: {}", position.getCurrentPrice());
+		logger.debug("        P&L: {}", position.getProfitAndLoss());
+		logger.debug("Used Margin: {}", position.getUsedMargin());
+	}
+	
+	private void dumpSecurityState(Security security) {
+		logger.debug("Security state ----------------------------------");
+		logger.debug("       Name: {}", security.getDisplayName());
+		logger.debug("     Symbol: {}", security.getSymbol());
+		logger.debug(" Expiration: {}", security.getExpirationTime());
+		logger.debug("   Lot Size: {}", security.getLotSize());
+		logger.debug("  Tick Size: {}", security.getTickSize());
+		logger.debug("  Tick Val.: {}", security.getTickValue());
+		logger.debug("         IM: {}", security.getInitialMargin());
+		logger.debug("Upper Price: {}", security.getUpperPriceLimit());
+		logger.debug("Lower Price: {}", security.getLowerPriceLimit());
+		logger.debug("Setl. Price: {}", security.getSettlementPrice());
+		logger.debug("   Best Ask: {}", security.getBestAsk());
+		logger.debug("   Best Bid: {}", security.getBestBid());
+		logger.debug(" Last Trade: {}", security.getLastTrade());
+		logger.debug("       Open: {}", security.getOpenPrice());
+		logger.debug("       High: {}", security.getHighPrice());
+		logger.debug("        Low: {}", security.getLowPrice());
+		logger.debug("      Close: {}", security.getClosePrice());
 	}
 
 }
