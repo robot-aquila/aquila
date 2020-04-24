afa6ab741e2607de5853670505e73cc3df75633b
aquila <7854540+robot-aquila@users.noreply.github.com>
2020-04-24 20:25:06 +0300

    More waits in Fidexp IT

--- web-utils/src/test/java/ru/prolib/aquila/web/utils/finam/FidexpIT.java
+++ web-utils/src/test/java/ru/prolib/aquila/web/utils/finam/FidexpIT.java
@@ -14,6 +14,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -83,6 +84,7 @@ public class FidexpIT {
 	@After
 	public void tearDown() throws Exception {
 		IOUtils.closeQuietly(facade);
+		Thread.sleep(ThreadLocalRandom.current().nextLong(1000L, 1000L));
 	}
 	
 	private LocalDate nextFuturesDate(LocalDate date) {
